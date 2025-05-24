f66a3a4c7527fafc9416303ef1c6167622a712d7
Neeraj Negi <neeraj33negi@gmail.com>
2025-05-24 20:29:31 +0530

    fix bug

--- lib/time_window_server/middlewares/time_limiter.rb
+++ lib/time_window_server/middlewares/time_limiter.rb
@@ -10,7 +10,7 @@ module TimeWindowServer
         current_time = Time.now.in_time_zone(@config.time_zone)
         current_hour = current_time.hour
 
-        if current_hour < @config.start_hour || current_time > @config.end_hour
+        if current_hour < @config.start_hour || current_hour > @config.end_hour
           body = { status: :unvailable, message: "Available b/w hours #{@config.start_hour} to #{@config.end_hour}" }.to_json
           [
             503,
